496449f6bcef2cd5ce2bce911ffe57be98d730b5
Moattar Ansari <moattaransari62@gmail.com>
2024-11-19 07:51:36 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,10 @@
+                    //type annotation.....
+
+//TypeScript mein type annotation ka matlab hota hai ke hum apne variables, functions, aur objects ke liye specify karte hain ke wo kis type ke data ko store karenge ya return karenge. Ye humari code ko samajhne aur maintain karne mein madad karta hai.TypeScript mein type annotation ka matlab hota hai ke hum apne variables, functions, aur objects ke liye specify karte hain ke wo kis type ke data ko store karenge ya return karenge. Ye humari code ko samajhne aur maintain karne mein madad karta hai.
+
+let num: number;
+
+// Is example mein `num` variable ko `number` type ka declare kiya gaya hai, matlab ke ismein sirf numbers store ho sakte hain.
+
+//TypeScript mein type annotation ka matlab hota hai ke hum apne variables, functions, aur objects ke liye specify karte hain ke wo kis type ke data ko store karenge ya return karenge. Ye humari code ko samajhne aur maintain karne mein madad karta hai.
+
